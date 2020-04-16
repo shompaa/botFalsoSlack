a801a1b40edfcf119c5e1e7acfa4f1c2518b7799
shompa <nico_nahuelpan@hotmail.com>
2020-04-15 23:00:01 -0400

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SlackAPI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+
+namespace BotFalsoSlack
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private static async Task EnviarMensajeAlBot(string mensaje)
+        {
+            const string TOKEN = "xoxb-1078280749793-1064282359525-XpjVVS9IT2btexCd4nYZLC87";  // token from last step in section above
+            var slackClient = new SlackTaskClient(TOKEN);
+            
+            var response = await slackClient.PostMessageAsync("#botfalso", mensaje);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await EnviarMensajeAlBot(textBox.Text.ToString());
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Bot Falso Slack
+_Este es un bot falso de slack _
+
+## Comenzando 🚀
+
+
+
+Mira **Deployment** para conocer como desplegar el proyecto.
+
+
+### Pre-requisitos 📋
+
+_Recuerda crear al app de slack en 
+
+```
+https://medium.com/@justinshawngerber/how-to-create-a-simple-slackbot-in-c-f42b90bb8982._
+
+```
+
+## Construido con 🛠️
+
+_Menciona las herramientas que utilizaste para crear tu proyecto_
+
+* [Slack](http://slack.com//) - Slack
+* [Mahapps](https://mahapps.com//) - WPF Style
+
+## Autores ✒️
+
+_Menciona a todos aquellos que ayudaron a levantar el proyecto desde sus inicios_
+
+* **Nicolas Nahuelpan** - *Trabajo Inicial* - [Shompaa](https://github.com/shompaa)
+* **Giovanni Molina** - *Documentación* - [Giovasdf](https://github.com/Giovasdf)
+
+## Expresiones de Gratitud 🎁
+
+* Comenta a otros sobre este proyecto 📢
+* Invita una cerveza 🍺 o un café ☕ a alguien del equipo. 
+* Da las gracias públicamente 🤓.
+* etc.
+
